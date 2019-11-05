2a09a4b9e514954be99ce2b9b242f33ebfbc07fd
Ubuntu <ubuntu@ip-172-31-17-81.ec2.internal>
2019-11-05 17:58:54 +0000

    added program

---
+++ nested_loops.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Created by: Liam Hearty
+# Created on: September 2019
+# This program adds only positive numbers.
+
+
+def main():
+    # this function uses a nested loop
+
+    red = 0
+    green = 0
+    blue = 0
+
+    # process & output
+    for red in range(255+1):
+        for green in range(255+1):
+            for blue in range(255+1):
+                print("RGB {0}, {1}, {2}".format(red, green, blue))
+
+
+if __name__ == "__main__":
+    main()
